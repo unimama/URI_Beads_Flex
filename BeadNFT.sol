3046fb63c37128e73b4ea19cb9c5fb2aae1a9745
unimama <57190706+unimama@users.noreply.github.com>
2025-08-12 20:34:43 +0200

    PeqBeadNFT.sol

--- BeadNFT.sol
+++ BeadNFT.sol
@@ -2,13 +2,9 @@
 pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Pausable.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
-import "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
-import "./BeadNFTValidator.sol";
 
 interface IBeadNFTValidator {
     function checkValidation(string memory beadId) external view returns (bool isValidated, bool isValid);
@@ -17,18 +13,8 @@ interface IBeadNFTValidator {
     function storeBeadURI(string calldata beadId, string calldata uri) external;
 }
 
-contract BeadNFT is ERC721, ConfirmedOwner, ReentrancyGuard, AccessControl, Pausable {
+contract BeadNFT is ERC721, AccessControl, ReentrancyGuard, Pausable {
     using Strings for uint256;
-
-    address private constant LINK_TOKEN = 0x779877A7B0D9E8603169DdbD7836e478b4624789;
-    LinkTokenInterface private constant LINK = LinkTokenInterface(LINK_TOKEN);
-
-    BeadNFTValidator public validator;
-
-    event BeadCreated(string beadId, string sku);
-    event BeadPreMinted(string beadId, string uri, uint256 timestamp);
-    event BeadMinted(string beadId, uint256 tokenId, address owner);
-    event BeadURIUpdated(string beadId, uint256 tokenId, string newUri, address updater);
 
     struct Bead {
         string beadId;
@@ -36,10 +22,6 @@ contract BeadNFT is ERC721, ConfirmedOwner, ReentrancyGuard, AccessControl, Paus
         uint256 preMintDate;
         string sku;
     }
-
-    uint256 private constant MAX_URI_HISTORY = 100;
-    uint256 private constant MAX_BATCH_SIZE = 500;
-    uint256 private constant MAX_QUERY_RESULT = 200;
 
     mapping(string => Bead) private beads;
     mapping(string => bool) private beadExists;
@@ -50,42 +32,29 @@ contract BeadNFT is ERC721, ConfirmedOwner, ReentrancyGuard, AccessControl, Paus
     uint256 private totalMinted;
     uint256 private nextTokenId = 1;
 
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
-
-    address[] public authorizedWallets;
     mapping(address => bool) public isAuthorizedWallet;
 
-    constructor() ERC721("BeadNFT", "BEAD") ConfirmedOwner(msg.sender) {
-        address router = 0xb83E47C2bC239B3bf370bc41e1459A34b41238D0;
-        bytes32 donId = 0x66756e2d657468657265756d2d7365706f6c69612d3100000000000000000000;
+    // Validator will be set after deployment
+    address public validatorAddress;
 
-        validator = new BeadNFTValidator(router, donId);
-        validator.grantBeadNFTRole(address(this));
+    event BeadCreated(string beadId, string sku);
+    event BeadPreMinted(string beadId, string uri, uint256 timestamp);
+    event BeadMinted(string beadId, uint256 tokenId, address owner);
 
+    constructor() ERC721("BeadNFT", "BEAD") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(ADMIN_ROLE, msg.sender);
-        _grantRole(PAUSER_ROLE, msg.sender);
-
-        authorizedWallets.push(msg.sender);
         isAuthorizedWallet[msg.sender] = true;
-    }
-
-    modifier validateURI(string memory uri) {
-        require(bytes(uri).length > 0, "URI cannot be empty");
-        require(bytes(uri).length <= 2048, "URI too long");
-        _;
-    }
-
-    modifier validateBeadId(string memory beadId) {
-        require(bytes(beadId).length > 0, "Bead ID cannot be empty");
-        require(bytes(beadId).length <= 32, "Bead ID too long");
-        _;
     }
 
     modifier onlyAuthorized() {
         require(isAuthorizedWallet[msg.sender] || hasRole(ADMIN_ROLE, msg.sender), "Not authorized");
         _;
+    }
+
+    function setValidator(address _validator) external onlyRole(ADMIN_ROLE) {
+        validatorAddress = _validator;
     }
 
     function generateBeadId() internal returns (string memory) {
@@ -102,88 +71,57 @@ contract BeadNFT is ERC721, ConfirmedOwner, ReentrancyGuard, AccessControl, Paus
     function _randomBeadId(uint256 _localNonce) internal view returns (string memory) {
         bytes memory charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         bytes memory beadIdBytes = new bytes(8);
-        unchecked {
-            for (uint256 i = 0; i < 8; i++) {
-                beadIdBytes[i] = charset[
-                    uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, _localNonce + i))) % charset.length
-                ];
-            }
+        for (uint256 i = 0; i < 8; i++) {
+            beadIdBytes[i] = charset[uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, _localNonce + i))) % charset.length];
         }
         return string(beadIdBytes);
     }
 
     function createBeadIds(string memory sku, uint256 qty) external onlyAuthorized whenNotPaused nonReentrant {
-        require(qty > 0, "Quantity must be greater than zero");
-        require(qty <= MAX_BATCH_SIZE, "Batch size too large");
+        require(qty > 0 && qty <= 100, "Invalid quantity");
         require(bytes(sku).length > 0, "SKU cannot be empty");
-        require(bytes(sku).length <= 64, "SKU too long");
-
-        string[] memory tempIds = new string[](qty);
-        for (uint256 i = 0; i < qty; i++) {
-            tempIds[i] = generateBeadId();
-        }
 
         for (uint256 i = 0; i < qty; i++) {
-            beads[tempIds[i]] = Bead(tempIds[i], new string[](0), 0, sku);
-            beadList.push(tempIds[i]);
-            emit BeadCreated(tempIds[i], sku);
+            string memory beadId = generateBeadId();
+            beads[beadId] = Bead(beadId, new string[](0), 0, sku);
+            beadList.push(beadId);
+            emit BeadCreated(beadId, sku);
         }
     }
 
-    function listBeads(uint256 offset, uint256 limit) external view returns (Bead[] memory) {
-        require(limit <= MAX_QUERY_RESULT, "Result set too large");
-        if (offset >= beadList.length && beadList.length > 0) {
-            revert("Offset out of bounds");
-        }
+    function preMint(string memory beadId, string memory uri) external onlyAuthorized whenNotPaused nonReentrant {
+        require(beadExists[beadId], "Bead ID does not exist");
+        require(beads[beadId].preMintDate == 0, "Bead already pre-minted");
+        require(bytes(uri).length > 0, "URI cannot be empty");
 
-        uint256 resultSize = 0;
-        if (beadList.length > offset) {
-            resultSize = (offset + limit > beadList.length) ? beadList.length - offset : limit;
+        beads[beadId].uris.push(uri);
+        beads[beadId].preMintDate = block.timestamp;
+        
+        if (validatorAddress != address(0)) {
+            IBeadNFTValidator(validatorAddress).storeBeadURI(beadId, uri);
         }
-
-        Bead[] memory beadArray = new Bead[](resultSize);
-        for (uint256 i = 0; i < resultSize; i++) {
-            beadArray[i] = beads[beadList[offset + i]];
-        }
-        return beadArray;
-    }
-
-    function getTotalBeads() external view returns (uint256) {
-        return beadList.length;
+        
+        emit BeadPreMinted(beadId, uri, block.timestamp);
     }
 
     function triggerMetadataValidation(uint64 subscriptionId, string calldata beadId) external onlyAuthorized whenNotPaused returns (bytes32 requestId) {
         require(beadExists[beadId], "Bead ID does not exist");
-        return validator.triggerMetadataValidation(subscriptionId, beadId);
+        require(validatorAddress != address(0), "Validator not set");
+        return IBeadNFTValidator(validatorAddress).triggerMetadataValidation(subscriptionId, beadId);
     }
 
-    function getValidationStatus(string memory beadId) external view returns (bool wasValidationAttempted, bool isCurrentlyValid) {
-        require(beadExists[beadId], "Bead ID does not exist");
-        return validator.getValidationStatus(beadId);
-    }
-
-    function preMint(string memory beadId, string memory uri) external onlyAuthorized whenNotPaused validateBeadId(beadId) validateURI(uri) nonReentrant {
-        require(beadExists[beadId], "Bead ID does not exist");
-        require(beads[beadId].preMintDate == 0, "Bead already pre-minted");
-
-        beads[beadId].uris.push(uri);
-        beads[beadId].preMintDate = block.timestamp;
-        validator.storeBeadURI(beadId, uri);
-        emit BeadPreMinted(beadId, uri, block.timestamp);
-    }
-
-    function mintBead(string memory beadId) external onlyAuthorized whenNotPaused validateBeadId(beadId) nonReentrant {
+    function mintBead(string memory beadId) external onlyAuthorized whenNotPaused nonReentrant {
         require(beadExists[beadId], "Bead ID does not exist");
         require(beads[beadId].preMintDate > 0, "Bead must be pre-minted first");
         require(beadIdToTokenId[beadId] == 0, "Bead already minted");
 
-        (bool isValidated, bool isValid) = validator.checkValidation(beadId);
-        require(isValidated, "Metadata validation not yet performed/completed for this beadId.");
-        require(isValid, "Metadata validation failed for this beadId.");
+        if (validatorAddress != address(0)) {
+            (bool isValidated, bool isValid) = IBeadNFTValidator(validatorAddress).checkValidation(beadId);
+            require(isValidated, "Metadata validation not yet performed");
+            require(isValid, "Metadata validation failed");
+        }
 
-        uint256 tokenId = nextTokenId;
-        nextTokenId++;
-
+        uint256 tokenId = nextTokenId++;
         _mint(msg.sender, tokenId);
         tokenIdToBeadId[tokenId] = beadId;
         beadIdToTokenId[beadId] = tokenId;
@@ -191,70 +129,45 @@ contract BeadNFT is ERC721, ConfirmedOwner, ReentrancyGuard, AccessControl, Paus
         emit BeadMinted(beadId, tokenId, msg.sender);
     }
 
-    function getBead(string memory beadId) external view validateBeadId(beadId) returns (Bead memory) {
+    function getBead(string memory beadId) external view returns (Bead memory) {
         require(beadExists[beadId], "Bead ID does not exist");
         return beads[beadId];
     }
 
-    function totalSupply() external view returns (uint256) {
-        return totalMinted;
+    function listBeads(uint256 offset, uint256 limit) external view returns (Bead[] memory) {
+        require(limit <= 50, "Limit too large");
+        if (offset >= beadList.length) return new Bead[](0);
+        
+        uint256 resultSize = (offset + limit > beadList.length) ? beadList.length - offset : limit;
+        Bead[] memory result = new Bead[](resultSize);
+        
+        for (uint256 i = 0; i < resultSize; i++) {
+            result[i] = beads[beadList[offset + i]];
+        }
+        return result;
+    }
+
+    function getTotalBeads() external view returns (uint256) {
+        return beadList.length;
     }
 
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         _requireOwned(tokenId);
         string memory beadId = tokenIdToBeadId[tokenId];
-        require(bytes(beadId).length > 0, "BeadNFT: URI query for nonexistent bead");
-
         string[] memory uris = beads[beadId].uris;
-        require(uris.length > 0, "BeadNFT: No URI set for this bead");
+        require(uris.length > 0, "No URI set");
         return uris[uris.length - 1];
     }
 
-    function getTokenIdByBeadId(string memory beadId) public view returns (uint256) {
-        require(beadExists[beadId], "Bead ID does not exist");
-        require(beadIdToTokenId[beadId] != 0, "Bead not yet minted");
-        return beadIdToTokenId[beadId];
-    }
-
-    function updateBeadURI(string memory beadId, string memory newUri) external onlyAuthorized whenNotPaused validateBeadId(beadId) validateURI(newUri) nonReentrant {
-        require(beadExists[beadId], "Bead ID does not exist");
-        require(beadIdToTokenId[beadId] != 0, "Bead must be minted first");
-
-        uint256 tokenId = beadIdToTokenId[beadId];
-        require(_ownerOf(tokenId) != address(0), "BeadNFT: URI update for unminted bead");
-        require(beads[beadId].uris.length < MAX_URI_HISTORY, "Maximum URI history reached");
-
-        beads[beadId].uris.push(newUri);
-        emit BeadURIUpdated(beadId, tokenId, newUri, msg.sender);
-    }
-
-    function getBeadIdByTokenId(uint256 tokenId) external view returns (string memory) {
-        string memory beadId = tokenIdToBeadId[tokenId];
-        require(bytes(beadId).length > 0, "BeadNFT: Query for nonexistent token");
-        return beadId;
-    }
-
-    function isBeadMinted(string memory beadId) external view returns (bool) {
-        require(beadExists[beadId], "Bead ID does not exist");
-        return beadIdToTokenId[beadId] != 0;
-    }
-
     function getValidatorAddress() external view returns (address) {
-        return address(validator);
+        return validatorAddress;
     }
 
-    function addAuthorizedWallet(address wallet) external onlyRole(ADMIN_ROLE) {
-        require(wallet != address(0), "Invalid wallet address");
-        require(!isAuthorizedWallet[wallet], "Wallet already authorized");
-        authorizedWallets.push(wallet);
-        isAuthorizedWallet[wallet] = true;
-    }
-
-    function pause() external onlyRole(PAUSER_ROLE) {
+    function pause() external onlyRole(ADMIN_ROLE) {
         _pause();
     }
 
-    function unpause() external onlyRole(PAUSER_ROLE) {
+    function unpause() external onlyRole(ADMIN_ROLE) {
         _unpause();
     }
 
